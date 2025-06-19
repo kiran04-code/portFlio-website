7022f728841f1a302d933369bdd07d6554945230
kiran04-code <kr551344@gmail.com>
2025-06-19 19:52:54 +0530

    Fix: Corrected file path casing for Render (Linux)

--- my-react-app/src/components/project.jsx
+++ my-react-app/src/components/project.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Project1 from './projectscomponents/Project1'
+import Project1 from './projectscomponents/project1'
 import { NavLink } from 'react-router-dom'
 
 const Project = () => {
